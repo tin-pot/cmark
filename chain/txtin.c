8ebe8906cf8ab0698e0664e20e409d464d20ce3e
tin-pot <mh@tin-pot.net>
2015-10-25 04:34:20 +0100

    chain / txtin.c : Here is the long outstanding "wrap text into ESIS document" tool
    Trivial as expected.

--- chain/txtin.c
+++ chain/txtin.c
@@ -1,9 +1,35 @@
 /* txtin.c */
 
 #include <stdio.h>
+#include <esisio.h>
+
+ESIS_Writer writer;
+
+void pump(void)
+{
+    char buffer[BUFSIZ];
+    size_t nread;
+    
+    while ((nread = fread(buffer, 1, BUFSIZ, stdin)) > 0U)
+      ESIS_Cdata(writer, buffer, nread);
+}
 
 int main(void)
 {
-    puts("txtin is not yet implemented -- at all!");
+    const char *atts[] = {
+      "mode", "vertical",
+      "notation", "CommonMark",
+      "label", "plain-text input",
+      NULL
+    };
+    
+    writer = ESIS_WriterCreate(stdout, 0U);
+    puts("?xml version=\"1.0\" encoding=\"UTF-8\"");
+    ESIS_Start(writer, "mark-up", atts);
+    pump();
+    ESIS_End(writer, "mark-up");
+    
+    ESIS_WriterFree(writer);
+    
     return 0;
 }
