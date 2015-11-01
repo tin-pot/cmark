96e1a50a36c7ad2cea695fa239338d345336b0f4
tin-pot <mh@tin-pot.net>
2015-11-01 21:09:26 +0100

    EXT: txtin does now some (hard-wired) filtering of "foreign syntax blocks".
    This is obviously preliminary:
    
     1. It needs to be configurable;
     2. it is (also) the job of `cmark_filter` (if `txtin` is not around) to recognize "foreign syntax" fragments.

--- chain/txtin.c
+++ chain/txtin.c
@@ -1,37 +1,102 @@
 /* txtin.c */
 
 #include <stdio.h>
+#include <string.h>
 #include <esisio.h>
 
 ESIS_Writer writer;
 
 #define LINELEN 32
 
+static const struct def {
+  const char *start_pattern;
+  const char *end_pattern;
+  const char *syntax;
+} defs[] = {
+  { "+-- ", "---\n", "z" },
+  { "+== ", "-==\n", "z" },
+  { "+.. ", "-..\n", "z" },
+  { "%%Z",  "%%\n",  "z" },
+  { "```", "```\n",  NULL },
+  { NULL, NULL, NULL }
+};
+
+static const char *pline = NULL; /* HACK */
+
+int start(const char *line)
+{
+  int i;
+
+  pline = line;
+  for (i = 0; defs[i].end_pattern != NULL; ++i) {
+    size_t n = strlen(defs[i].start_pattern);
+    if (strncmp(line, defs[i].start_pattern, n) == 0)
+      return i;
+  }
+  return -1;
+}
+
+const char *syntax(int i)
+{
+  size_t n, m;
+  static char buf[32];
+
+  if (defs[i].syntax != NULL)
+    return defs[i].syntax;
+  n = strspn(pline, "` \t");
+  if (pline[n] == '\0')
+    return "code";
+  
+  m = strcspn(pline + n, "\n\t ");
+  if (m + 1 > sizeof buf)
+    m = sizeof buf - 1;
+  strncpy(buf, pline + n, m);
+  return buf;
+}
+
+int end(int i, const char *line)
+{
+  size_t n = strlen(defs[i].end_pattern);
+  return strncmp(line, defs[i].end_pattern, n) == 0;
+}
+
 void pump(void)
 {
-    char buffer[LINELEN];
-    size_t nread;
-    
-    while ((nread = fread(buffer, 1, sizeof buffer, stdin)) > 0U)
-      ESIS_Cdata(writer, buffer, nread);
+  char buffer[BUFSIZ+1];
+
+  while (fgets(buffer, sizeof buffer, stdin) != NULL) {
+    int isyntax = start(buffer);
+    if (isyntax < 0)
+      ESIS_Cdata(writer, buffer, ESIS_NTS);
+    else {
+      ESIS_Attr(writer, "syntax", syntax(isyntax), ESIS_NTS);
+      ESIS_Attr(writer, "mode", "block", ESIS_NTS);
+      ESIS_Start(writer, "mark-up", NULL);
+      ESIS_Cdata(writer, buffer, ESIS_NTS);
+      while (fgets(buffer, sizeof buffer, stdin) != NULL) {
+	ESIS_Cdata(writer, buffer, ESIS_NTS);
+	if (end(isyntax, buffer)) break;
+      }
+      ESIS_End(writer, "mark-up");
+    }
+  }
 }
 
 int main(void)
 {
-    const char *atts[] = {
-      "mode", "vertical",
-      "notation", "CommonMark",
-      "label", "plain-text input",
-      NULL
-    };
-    
-    writer = ESIS_WriterCreate(stdout, 0U);
-    puts("?xml version=\"1.0\" encoding=\"UTF-8\"");
-    ESIS_Start(writer, "mark-up", atts);
-    pump();
-    ESIS_End(writer, "mark-up");
-    
-    ESIS_WriterFree(writer);
-    
-    return 0;
+  const char *atts[] = {
+    "syntax", "cmark",
+    "mode",   "block",
+    "label",  "typescript",
+    NULL
+  };
+
+  writer = ESIS_WriterCreate(stdout, 0U);
+  ESIS_Start(writer, "mark-up", atts);
+  pump();
+  ESIS_End(writer, "mark-up");
+
+  ESIS_WriterFree(writer);
+
+  return 0;
 }
