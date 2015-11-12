41131184b2d456e877397d46e23949d53de5bb19
tin-pot <mh@tin-pot.net>
2015-11-12 19:21:48 +0100

    cm2doc.c Head comment, 2-clause BSD license.

--- src/cm2doc.c
+++ src/cm2doc.c
@@ -1,9 +1,86 @@
-/*== cm2doc.c ========================================================*/
+/*== cm2doc.c =========================================================*
+
+NAME
+    cm2doc -
+	    .
+            .
+
+SYNOPSIS
+    cm2doc [ cmark-opts ] [(--title | -t) string] [(--css | -c) url]
+           [ --rast | { ( --repl | -r ) replfile } ] file ...
+
+DESCRIPTION
+	...
+	...
+	...
+
+	 1. ...
+
+	 2. ...
+	    ...
 
 
-/*====================================================================*/
+OPTIONS
+    --rast
+	...
 
-/* C90 header */
+    --repl
+    -r
+
+    --title
+    -t
+	...
+
+    --css
+    -c
+	...
+
+EXIT STATUS
+	...
+	...
+
+ENVIRONMENT
+	XXX	...
+		...
+
+BUGS
+	...
+	...
+	...
+
+
+------------------------------------------------------------------------
+
+COPYRIGHT NOTICE AND LICENSE
+
+Copyright (C) 2015 Martin Hofmann <mh@tin-pot.net>
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions 
+are met:
+
+   1. Redistributions of source code must retain the above copyright 
+      notice, this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copy-
+      right notice, this list of conditions and the following dis-
+      claimer in the documentation and/or other materials provided 
+      with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY 
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLU-
+DING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*=====================================================================*/
+
 #include <assert.h>
 #include <ctype.h> /* toupper() */
 #include <errno.h>
